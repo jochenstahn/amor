654251a194e9e4ee34a18cb4238be791f0b896e1
jochenstahn <jochen.stahn@psi.ch>
2026-02-20 09:43:08 +0100

    new version

--- eos/__init__.py
+++ eos/__init__.py
@@ -2,5 +2,5 @@
 Package to handle data redction at AMOR instrument to be used by __main__.py script.
 """
 
-__version__ = '3.0.7'
-__date__    = '2025-12-01'
+__version__ = '3.1.0'
+__date__    = '2026-02-20'
