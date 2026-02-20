f55d840b8e573c47773bb163a79c7eecbfc41f8d
Artur Glavic <artur.glavic@psi.ch>
2026-02-20 13:57:50 +0100

    new version

--- eos/__init__.py
+++ eos/__init__.py
@@ -2,5 +2,5 @@
 Package to handle data redction at AMOR instrument to be used by __main__.py script.
 """
 
-__version__ = '3.1.0'
-__date__    = '2026-02-20'
+__version__ = '3.1.1'
+__date__    = '2026-02-26'
